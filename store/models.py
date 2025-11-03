0ecb58d230bca5ff707c63a123831a631b99345f
Ibraheem902 <ibraheem.touma5@gmail.com>
2025-11-03 19:56:21 +0300

    product order and payment

--- store/models.py
+++ store/models.py
@@ -40,6 +40,7 @@ class Variation(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     variation_category = models.CharField(max_length=100,choices= variation_category_choice)
     variation_value = models.CharField(max_length=100)
+    stock = models.IntegerField()
     is_active = models.BooleanField(default=True)
     created_date = models.DateField(auto_now=True)
     
